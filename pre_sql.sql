78a95c940f28663051f914ea2bc242e47bf3998a
Irina Voron <yarutich.irina@gmail.com>
2021-10-21 20:19:46 +0300

    try

---
+++ pre_sql.sql
@@ -0,0 +1,119 @@
+insert into roles (id, role_title)
+values (default, 'General Dentist');
+
+insert into roles (id, role_title)
+values (default, 'Orthodontist');
+
+insert into roles (id, role_title)
+values (default, 'Periodontist');
+
+insert into roles (id, role_title)
+values (default, 'Dental Assistant');
+
+insert into roles (id, role_title)
+values (default, 'Dental Hygienist');
+
+insert into roles (id, role_title)
+values (default, 'Laboratory Technician');
+
+insert into roles (id, role_title)
+values (default, 'Medical receptionist');
+
+insert into roles (id, role_title)
+values (default, 'Oral and maxillofacial surgeon');
+
+insert into roles (id, role_title)
+values (default, 'Orthodontic assistant');
+
+insert into roles (id, role_title)
+values (default, 'Endodontist');
+
+insert into employees (id, employee_name)
+values (default, 'Smith');
+
+insert into employees (id, employee_name)
+values (default, 'Johnson');
+
+insert into employees (id, employee_name)
+values (default, 'Williams');
+
+insert into employees (id, employee_name)
+values (default, 'Brown');
+
+insert into employees (id, employee_name)
+values (default, 'Jones');
+
+insert into employees (id, employee_name)
+values (default, 'Garcia');
+
+insert into employees (id, employee_name)
+values (default, 'Miller');
+
+insert into employees (id, employee_name)
+values (default, 'Davis');
+
+insert into employees (id, employee_name)
+values (default, 'Rodriguez');
+
+insert into employees (id, employee_name)
+values (default, 'Martinez');
+
+insert into salary (id, monthly_salary)
+values (default, 1000);
+
+insert into salary (id, monthly_salary)
+values (default, 1100);
+
+insert into salary (id, monthly_salary)
+values (default, 1200);
+
+insert into salary (id, monthly_salary)
+values (default, 1300);
+
+insert into salary (id, monthly_salary)
+values (default, 1400);
+
+insert into salary (id, monthly_salary)
+values (default, 1500);
+
+insert into salary (id, monthly_salary)
+values (default, 500);
+
+insert into salary (id, monthly_salary)
+values (default, 800);
+
+insert into salary (id, monthly_salary)
+values (default, 900);
+
+insert into salary (id, monthly_salary)
+values (default, 2000);
+
+insert into roles_salary (id, id_role, id_salary)
+values (default, 1, 10);
+
+insert into roles_salary (id, id_role, id_salary)
+values (default, 2, 9);
+
+insert into roles_salary (id, id_role, id_salary)
+values (default, 3, 8);
+
+insert into roles_salary (id, id_role, id_salary)
+values (default, 4, 7);
+
+insert into roles_salary (id, id_role, id_salary)
+values (default, 5, 6);
+
+insert into roles_salary (id, id_role, id_salary)
+values (default, 6, 5);
+
+insert into roles_salary (id, id_role, id_salary)
+values (default, 7, 4);
+
+insert into roles_salary (id, id_role, id_salary)
+values (default, 8, 3);
+
+insert into roles_salary (id, id_role, id_salary)
+values (default, 9, 2);
+
+insert into roles_salary (id, id_role, id_salary)
+values (default, 10, 1);
